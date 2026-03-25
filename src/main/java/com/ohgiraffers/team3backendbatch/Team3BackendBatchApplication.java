8a6d42e12b91843bae644beb6d1fdde98e210156
rlatlsdn98 <rlatlsdn98@naver.com>
2026-03-25 16:35:55 +0900

    init commit

---
+++ src/main/java/com/ohgiraffers/team3backendbatch/Team3BackendBatchApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.team3backendbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team3BackendBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team3BackendBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ohgiraffers/team3backendbatch/Team3BackendBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.team3backendbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team3BackendBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
